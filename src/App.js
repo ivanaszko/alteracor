46ff1226c1a8443a7417c5d22d048a4b0ebc97de
ivanaszko <leandro@ivanaszko.com.br>
2022-10-19 15:03:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './components/TesteImporCores.css';
+import {useState} from 'react'
+import TesteImporCores from './components/TesteImporCores';
+
+
+
+function App() {
+  
+  return (
+    <div className='container'>
+      <TesteImporCores />
+    </div>
+  );
+}
+
+export default App;
